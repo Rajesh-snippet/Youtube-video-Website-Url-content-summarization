dda5ce103788cdc5c49f3e87a885035279f2214a
Rajesh-snippet <rsarmabordoloi@gmail.com>
2026-03-28 22:07:11 +0530

    modified app.py

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ from langchain.agents import create_react_agent, AgentExecutor
 from langchain import hub
 from langchain.callbacks import StreamlitCallbackHandler
 from dotenv import load_dotenv
+
+load_dotenv()
+
+## Arxiv and Wikipedia Tools
+arxiv_wrapper = ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=200)
+arxiv = ArxivQueryRun(api_wrapper=arxiv_wrapper)
+
+api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=200)
+wiki = WikipediaQueryRun(api_wrapper=api_wrapper)
